416661985e1800600954b29eb076035e1182cb86
Freya <elodie.chllr@gmail.com>
2025-01-21 23:24:14 +0100

    Debut du jeu

--- Assets/Code/Scripts/Player/PlayerController.cs
+++ Assets/Code/Scripts/Player/PlayerController.cs
@@ -1,24 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb_player;
-    private float playerSpeed;
+    public float horizontalSpeed = 200f;
+    private float verticalSpeed = 1f;
+
+    Vector2 moveInput;
+    PlayerMap playerMapRef;
+
+    InputAction movementAction;
+    public PlayerInput playerInput;
     void Start()
     {
-        
+        rb_player = GetComponent<Rigidbody2D>();
+        playerMapRef = new PlayerMap();
+
+
+
+        movementAction = playerInput.actions["Movement"];
     }
 
     void Update()
     {
-        
+        PlayerMovement();
     }
 
 
     public void PlayerMovement()
     {
-
+        Vector2 movement = movementAction.ReadValue<Vector2>();
+        rb_player.velocity = new Vector2(movement.x * horizontalSpeed, verticalSpeed);
     }
 }
